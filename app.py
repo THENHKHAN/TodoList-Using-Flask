9d6d793c9d0ace6089f50f3e453003fd590a74a9
Noorul Huda Khan <noorulhuda101099@gmail.com>
2023-06-18 16:51:45 +0530

    Final completed but in future i may make changes

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # COnfuguring  DB and sqlalchemy with db-sqlite
-# create the extension
-db = SQLAlchemy()
+
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db" #/// mean relative path and //// i.e. 4 slash used for absolute path
 db = SQLAlchemy(app)
@@ -52,7 +51,62 @@ def homeCheck() :
 # above are just routing testing . And now below the real logic
 
 #  creating model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+
+# routes 
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
